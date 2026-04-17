61a081dcc32dad39131d8b382dbdcf17a453f8bc
qver1sy2023 <nikitaprotopopov2012@mail.ru>
2026-04-18 01:21:26 +0900

    Update !!!rentless_dev.lua

--- !!!rentless_dev.lua
+++ !!!rentless_dev.lua
@@ -17,7 +17,7 @@ if not LPH_OBFUSCATED then
 end
 
 local relentless_data = {} do
-    relentless_data.name = _USER_NAME or "admin"
+    relentless_data.name = _G._USER_NAME or "admin"
     relentless_data.update = "владивосток_россия_2026"
 end
 
